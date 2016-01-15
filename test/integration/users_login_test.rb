64ae4b386dc4922ebd4722d486a8a09c2faab297
Johnny Hu <jgeehen.hu@gmail.com>
2016-01-15 23:30:08 +0000

    Create persistent login session logic; start remember me logic

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -28,6 +28,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
     assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
+    follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
